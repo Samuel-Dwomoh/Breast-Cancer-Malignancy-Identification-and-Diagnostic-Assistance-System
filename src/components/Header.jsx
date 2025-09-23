6b11ca96b6c955cae9e0abd92fc26063eca12280
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-09-23 17:37:39 +0000

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,47 @@
 import React from "react";
 import logo from "../assets/logo.png"
+import { BackgroundLines } from "./ui/background-lines";
+
 
 function Header(){
 
 
 
     return(
-    <div className="flex mx-10 justify-between">
+        <>
+    <div className="flex mx-12 justify-between">
 
         <div className="flex items-center gap-x-4">
             <a href="#"><img src={logo} alt="logo" className="w-19" /></a>
-            <h4 className="uppercase text-pink-500 font-semibold">Breast Cancer Malignancy Identification and Diagnostic Assistance System</h4>
+            <h4 className="uppercase text-pink-500">Breast Cancer Malignancy Identification and Diagnostic Assistance System</h4>
         </div>
 
-        <nav className="flex gap-3 items-center text-pink-500 font-semibold">
+        <nav className="flex gap-6 items-center text-pink-500">
             <ul><a href="#">Diagnostic Interface</a></ul>
             <ul><a href="#">Results Dashboard</a></ul>
             <ul><a href="#">Clinical Evidence</a></ul>
         </nav>
+        
 
-    </div>)
+    </div>
+        
+
+
+
+           {/* <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 -mt-16">
+      <h2
+        className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        Sanjana Airlines, <br /> Sajana Textiles.
+      </h2>
+      <p
+        className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+        Get the best advices from our experts, including expert artists,
+        painters, marathon enthusiasts and RDX, totally free.
+      </p>
+    </BackgroundLines> */}
+
+    
+    </>
+    )
 }
 export default Header;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // Node.js path module for resolving paths
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Maps '@' to './src'
+    },
+  },
+});
