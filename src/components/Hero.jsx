80573862e0d1a4697e150620174e636315db697f
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-09-25 19:25:30 +0000

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ function Hero(){
         <section className="mt-60 px-4 absolute z-50 w-full">
 
             <div className="flex justify-center text-center">
-                <h3 className="text-pink-500 text-6xl font-bold tracking-wide p-4">AI-Powered Breast <br/>
-                <span className="text-pink-500 block mt-1 text-center"><span className="">Cancer</span> Detection</span></h3>
+                <h3 className="text-pink-500 text-6xl font-bold tracking-wide p-4">AI-Powered Breast Cancer<br/>
+                <span className="text-pink-500 block mt-1 text-center">Detection Assistant</span></h3>
             </div>
 
             <div className="flex justify-center text-center mt-1">
@@ -24,7 +24,7 @@ function Hero(){
 
             <div className="flex justify-center gap-x-6 mt-8 text-white">
                 <a href="" className="bg-pink-500 px-3 py-2 rounded-sm flex gap-x-2 hover:scale-105 transition delay-75">Try Model <img src={ArrowRight} alt="arrowRightLogo" /></a>
-                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm p-2 hover:scale-105 delay-75 flex gap-x-2 ">View Documentation <img src={documentation} alt="documentationLogo"></img></a>
+                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm hover:scale-105 transition delay-75 flex gap-x-2 ">View Documentation <img src={documentation} alt="documentationLogo"></img></a>
             </div>
 
         </section>
