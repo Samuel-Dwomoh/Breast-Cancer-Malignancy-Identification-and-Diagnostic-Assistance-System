2aac775336b7366667db7acd5cb5e145149c7c46
Samuel Dwomoh <sdwomoh418@gmail.com>
2025-09-25 19:14:10 +0000

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from './Header';
 import { SpotlightPreview } from "./spotligh";
+import ArrowRight  from "../assets/arrowRight.svg";
 
 function Hero(){
 
     return(
         <>
-        <section className="mt-54 px-4 absolute z-50 w-full">
+        <section className="mt-60 px-4 absolute z-50 w-full">
 
             <div className="flex justify-center text-center">
                 <h3 className="text-pink-500 text-6xl font-bold tracking-wide p-4">AI-Powered Breast <br/>
@@ -19,11 +20,12 @@ function Hero(){
                     <span className="block">for better patient outcomes.</span>
                 </p>
             </div>
-            
+
             <div className="flex justify-center gap-x-4 mt-8 text-white">
-                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm">Try Model</a>
-                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm">View Documentation</a>
+                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm flex gap-x-2 p-2">Try Model <img src={ArrowRight} alt="" /></a>
+                <a href="" className="bg-pink-500 px-3 py-2 rounded-sm p-2">View Documentation</a>
             </div>
+
         </section>
         </>
     );
--- src/components/spotligh.jsx
+++ src/components/spotligh.jsx
@@ -7,12 +7,12 @@ import Hero from './Hero';
 export function SpotlightPreview() {
   return (<>
     <div
-      className="relative h-[40rem] w-full overflow-hidden bg-black/[0.96] antialiased md:items-center md:justify-center">
+      className="relative h-[46.6rem] w-full overflow-hidden bg-black/[0.96] antialiased md:items-center md:justify-center">
           <Header / >
       <div
         className={cn(
           "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
-          "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]"
+          "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)] opacity-50"
         )} />
       <Spotlight className="-top-40 left-0 md:-top-20 md:left-60" fill="white" />
       <Hero />
